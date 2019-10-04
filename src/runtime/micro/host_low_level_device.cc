6201f93adb78f7793807421947d8387dee5987b0
Logan Weber <weberlo@cs.washington.edu>
2019-10-03 21:06:15 -0700

    TEMP 2

--- src/runtime/micro/host_low_level_device.cc
+++ src/runtime/micro/host_low_level_device.cc
@@ -73,6 +73,8 @@ class HostLowLevelDevice final : public LowLevelDevice {
     reinterpret_cast<void (*)(void)>(func_addr.value())();
   }
 
+  void Connect() {}
+
   std::uintptr_t base_addr() const final {
     return base_addr_;
   }
