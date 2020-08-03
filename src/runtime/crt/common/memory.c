246ae9ae55cfd4d9bdfceed79396b28177b15167
Andrew Reusch <areusch@octoml.ai>
2020-08-03 14:41:23 -0700

    fix compilation error

--- src/runtime/crt/common/memory.c
+++ src/runtime/crt/common/memory.c
@@ -139,7 +139,7 @@ void* MemoryManager_Alloc(MemoryManager* mgr, tvm_index_t size) {
   } else {
     start = ptable->num_pages;
     CHECK_LE((unsigned)(start + npage), ptable->max_pages,
-             "insufficient memory, start=%" PRId32 ", npage=%" PRId32 ", total=%" PRId32 " / %" PRId32, (int32_t) start,
+             "insufficient memory, start=%" PRId32 ", npage=%" PRId32 ", total=%" PRId32 " / %zu", (int32_t) start,
              (int32_t) npage, (int32_t) (start + npage), mgr->pmap.max_pages);
     /* insert page entry */
     Page p = PageCreate(ptable->memory_pool, ptable->page_size_bytes, start, npage);
