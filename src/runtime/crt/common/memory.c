918f4fa6d40bd1fa3886e61a34ecedb48ae60fc1
Andrew Reusch <areusch@octoml.ai>
2020-08-03 11:10:24 -0700

    fix memory log message to display on nrf

--- src/runtime/crt/common/memory.c
+++ src/runtime/crt/common/memory.c
@@ -139,8 +139,8 @@ void* MemoryManager_Alloc(MemoryManager* mgr, tvm_index_t size) {
   } else {
     start = ptable->num_pages;
     CHECK_LE((unsigned)(start + npage), ptable->max_pages,
-             "insufficient memory, start=%" PRId64 ", npage=%" PRId64 ", total=%" PRId64 "", start,
-             npage, start + npage);
+             "insufficient memory, start=%" PRId32 ", npage=%" PRId32 ", total=%" PRId32 " / %" PRId32, (int32_t) start,
+             (int32_t) npage, (int32_t) (start + npage), mgr->pmap.max_pages);
     /* insert page entry */
     Page p = PageCreate(ptable->memory_pool, ptable->page_size_bytes, start, npage);
     ptable->resize(ptable, start + npage, &p);
